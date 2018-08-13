121f7ce2f79d708f50d1c77e0a11a1374fd5f4a1
Andrey Popov <andpop@mail.ru>
2018-08-14 00:15:35 +0300

    1st homework

---
+++ homework-0.php
@@ -0,0 +1,19 @@
+<?php
+//CONST testConstanta = 'asd';
+//$user_name = "Igor";
+//
+//function show_something(){ RETURN 'SOMETHING';}
+//
+//if (1==1) echo 'hi';
+
+const TESTCONSTANTA = 'asd';
+$user_name = "Igor";
+
+function show_something()
+{
+    return 'SOME`THING';
+}
+
+if (1==1) {
+    echo 'hi';
+}
