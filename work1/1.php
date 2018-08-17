a10249499346d35a00050a061d77bade3dcd6be9
Andrey Popov <andpop@mail.ru>
2018-08-17 15:10:37 +0300

    Work 1, task 2.

--- work1/1.php
+++ work1/1.php
@@ -1,6 +1,9 @@
+<pre>
 <?php
 $name = 'Андрей';
 $age = '43';
 echo "Меня зовут: $name".PHP_EOL;
 echo "Мне $age года".PHP_EOL;
 echo '"!|\/\'"\\'.PHP_EOL;
+?>
+</pre>
---
+++ work1/2.php
@@ -0,0 +1,6 @@
+<?php
+const TOTAL_FIGS = 80;
+const FELT_FIGS = 23;
+const PENCIL_FIGS = 40;
+$paintFigs = TOTAL_FIGS - FELT_FIGS - PENCIL_FIGS;
+echo "Написано красками: $paintFigs".PHP_EOL;
