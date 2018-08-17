f79fd6f980afc00688aba5e1c3688df3172c2e88
Andrey Popov <andpop@mail.ru>
2018-08-18 00:12:41 +0300

    Work 1, task 6.

--- work1/6.php
+++ work1/6.php
@@ -16,20 +16,49 @@
     <meta charset="UTF-8">
     <title>Задание #6</title>
 </head>
+<style>
+    table {
+        /*font-family: "Lucida Sans Unicode", "Lucida Grande", Sans-Serif;*/
+        font-size: 14px;
+        border-collapse: collapse;
+        text-align: center;
+    }
+    th, td:first-child {
+        background: #AFCDE7;
+        color: white;
+        padding: 10px 20px;
+    }
+    th, td {
+        border-style: solid;
+        border-width: 0 1px 1px 0;
+        border-color: white;
+    }
+    td {
+        background: #D8E6F3;
+    }
+</style>
 <body>
     <table>
         <tr>
         <?php
-        for ($i = 1; $i <= 10; $i++) {
-            echo "<td>$i</td>";
+        for ($rowCell = 1; $rowCell <= 10; $rowCell++) {
+            echo "<th>$rowCell</th>";
         };
         ?>
         </tr>
         <?php
-        for ($verticalCell = 2; $verticalCell <=10; $verticalCell++) {
+        for ($columnCell = 2; $columnCell <=10; $columnCell++) {
             echo "<tr>";
-            for ($horizontalCell = 1; $horizontalCell <= 10; $horizontalCell++) {
-                $result = $verticalCell * $horizontalCell;
+            for ($rowCell = 1; $rowCell <= 10; $rowCell++) {
+                $product = $columnCell * $rowCell;
+                if (($columnCell % 2 == 0) and ($rowCell % 2 == 0)) {
+                    $result = "({$product})";
+                } elseif (($columnCell % 2 == 1) and ($rowCell % 2 == 1)) {
+                    $result = "[{$product}]";
+                } else {
+                    $result = $product;
+                };
+
                 echo "<td>$result</td>";
             }
             echo "</tr>";
