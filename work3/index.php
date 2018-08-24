77f649866970debfb3cea7fd415b3fc970e4020f
Andrey Popov <andpop@mail.ru>
2018-08-24 16:08:36 +0300

    Work 3, task 1.

---
+++ work3/index.php
@@ -0,0 +1,81 @@
+<?php
+require('src/functions.php');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Домашнее задание #2</title>
+</head>
+<style>
+    table {
+        font-size: 14px;
+        border-collapse: collapse;
+        text-align: center;
+    }
+    th, td:first-child {
+        background: #AFCDE7;
+        color: white;
+        padding: 10px 20px;
+    }
+    th, td {
+        border-style: solid;
+        border-width: 0 1px 1px 0;
+        border-color: white;
+    }
+    td {
+        background: #D8E6F3;
+    }
+</style>
+
+<body>
+<h2>Задача 1</h2>
+<?php
+echo task1(["1 элемент", "2 элемент", "3 элемент"], true);
+//echo "<br>";
+echo task1("123");
+?>
+<hr>
+<h2>Задача 2</h2>
+<?php
+$result = task2("*", 2, 3, 4);
+if (is_numeric($result)) {
+    echo  $result.'<br>';
+} else {
+    echo  'Ошибка! '.$result.'<br>';
+};
+$result = task2_2("*", 2, 3, 4);
+if (is_numeric($result)) {
+    echo  $result.'<br>';
+} else {
+    echo  'Ошибка! '.$result.'<br>';
+};
+
+?>
+<hr>
+<h2>Задача 3</h2>
+<?php
+task3(7, 7);
+?>
+<hr>
+<h2>Задача 4</h2>
+<?php
+task4();
+?>
+<hr>
+<h2>Задача 5</h2>
+<?php
+task5();
+?>
+<hr>
+<h2>Задача 6</h2>
+<?php
+task6("test.txt");
+?>
+<hr>
+
+</body>
+</html>
+
+
+
---
+++ work3/src/functions.php
@@ -0,0 +1,20 @@
+<?php
+/*Purchase order N {}
+Date: {}
+Shipping address: {}
+Billing address: {}
+
+Delivery notes: {}
+
+1. PartNumber: {}
+    ProductName: {}
+    ...
+-------------------
+2. PartNumber: {}
+    ProductName: {}
+    ...
+-------------------*/
+function task1()
+{
+    echo '111111111111';
+}
