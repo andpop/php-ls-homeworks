d2e3b7bf0805a8715c70e3e05306284886d01b27
Andrey Popov <andpop@mail.ru>
2018-08-24 20:30:56 +0300

    Work 3, task 1.

--- work3/index.php
+++ work3/index.php
@@ -7,26 +7,6 @@ require('src/functions.php');
     <meta charset="UTF-8">
     <title>Домашнее задание #2</title>
 </head>
-<style>
-    table {
-        font-size: 14px;
-        border-collapse: collapse;
-        text-align: center;
-    }
-    th, td:first-child {
-        background: #AFCDE7;
-        color: white;
-        padding: 10px 20px;
-    }
-    th, td {
-        border-style: solid;
-        border-width: 0 1px 1px 0;
-        border-color: white;
-    }
-    td {
-        background: #D8E6F3;
-    }
-</style>
 
 <body>
 <h2>Задача 1</h2>
--- work3/src/functions.php
+++ work3/src/functions.php
@@ -1,32 +1,53 @@
 <?php
-/*Purchase order N {}
-Date: {}
-Shipping address: {}
-Billing address: {}
-
-Delivery notes: {}
-
-1. PartNumber: {}
-    ProductName: {}
-    ...
--------------------
-2. PartNumber: {}
-    ProductName: {}
-    ...
--------------------*/
+/**
+ * 1. Дан XML файл. Сохраните его под именем data.xml
+ * 2. Написать скрипт, который выведет всю информацию из этого файла в удобно
+ * читаемом виде. Представьте, что результат вашего скрипта будет распечатан и выдан
+ * курьеру для доставки, разберется ли курьер в этой информации?
+ */
 function task1()
 {
-    $file = file_get_contents("data.xml");
+    $file = file_get_contents("./data/data.xml");
     $xml = new SimpleXMLElement($file);
+//    Можно выделять атрибуты так:
 //    $orderNumber = $xml->attributes()->PurchaseOrderNumber;
 //    $orderDate = $xml->attributes()->OrderDate;
     $orderNumber = $xml['PurchaseOrderNumber'];
     $orderDate = $xml['OrderDate'];
+    $shippingAddress = $xml->xpath("//Address[@Type='Shipping']")[0];
+    $billingAddress = $xml->xpath("//Address[@Type='Billing']")[0];
+    $deliveryNotes = $xml->DeliveryNotes;
+    $items = $xml->Items->Item;
+//    var_dump($items);
 
-//    echo "<pre>";
-//    var_dump($xml);
-//    var_dump($xml->attributes()->PurchaseOrderNumber);
-    echo "Purchase order N {$orderNumber}<br>".PHP_EOL;
-    echo "Date: {$orderDate}<br>".PHP_EOL;
+    echo "<hr>".PHP_EOL;
+    echo "Purchase order: <strong>{$orderNumber}</strong>  Date: <strong>{$orderDate}</strong><br><br>".PHP_EOL;
+    echo "<strong>Shipping address:</strong><br>".PHP_EOL;
+    echo "{$shippingAddress->Name}<br>".PHP_EOL;
+    echo "{$shippingAddress->Street}<br>".PHP_EOL;
+    echo "{$shippingAddress->City}, {$shippingAddress->State}, {$shippingAddress->Zip}<br>".PHP_EOL;
+    echo "{$shippingAddress->Country}<br>".PHP_EOL;
+    echo "<br>";
+    echo "<strong>Billing address:</strong><br>".PHP_EOL;
+    echo "{$billingAddress->Name}<br>".PHP_EOL;
+    echo "{$billingAddress->Street}<br>".PHP_EOL;
+    echo "{$billingAddress->City}, {$billingAddress->State}, {$billingAddress->Zip}<br>".PHP_EOL;
+    echo "{$billingAddress->Country}<br>".PHP_EOL;
+    echo "<br>";
+    echo "Delivery notes: <i>{$deliveryNotes}</i><br><hr>".PHP_EOL;
 
+    $itemNumber = 0;
+    foreach ($items as $item) {
+        $itemNumber++;
+        echo "<strong>#{$itemNumber}</strong><br>".PHP_EOL;
+        echo "Part. number: {$item['PartNumber']}<br>".PHP_EOL;
+        echo "Product name: {$item->ProductName}<br>".PHP_EOL;
+        echo "Quantity: {$item->Quantity}<br>".PHP_EOL;
+        echo "Price: \${$item->USPrice}<br>".PHP_EOL;
+        echo "Ship date: {$item->ShipDate}<br>".PHP_EOL;
+        echo "Comment: {$item->Comment}<br>".PHP_EOL;
+//        var_dump($item);
+        echo "<hr>";
+    }
+    echo "Total: <strong>{$itemNumber} items</strong><br>".PHP_EOL;
 }
