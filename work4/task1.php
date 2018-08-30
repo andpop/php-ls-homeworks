8af4ed63193269b862a001687acd88416ec4e157
Andrey Popov <andpop@mail.ru>
2018-08-31 00:13:28 +0300

    Work 4, task 1. Add student constraint.

--- work4/task1.php
+++ work4/task1.php
@@ -36,18 +36,19 @@ abstract class A_Tariff implements I_Tariff
 {
     const MIN_AGE = 18;
     const MAX_AGE = 65;
-    const MIN_YOUTH_AGE = 18;
     const MAX_YOUTH_AGE = 21;
+    const MAX_STUDENT_AGE = 21;
     const YOUTH_RATE = 0.1;
 
     protected $tariffName;
     protected $pricePerKilometer;   // Цена за километр
-    protected $pricePerMinute;      // Цена за минуту
+    protected $pricePerTime;      // Цена за минуту
     protected $isGpsAvailable = true;
     protected $isAdditionalDriverAvailable = true;
 
     protected $distance, $hours, $minutes, $age, $isGPS = false, $isAdditionalDriver = false;
     protected $baseTripPrice, $additionalTripPrice, $totalTripPrice;
+    protected $errorMessage;
 
     use AdditionalService;
 
@@ -68,7 +69,12 @@ abstract class A_Tariff implements I_Tariff
 
     protected function isYouthAge($age)
     {
-        return ($age >= self::MIN_YOUTH_AGE && $age <= self::MAX_YOUTH_AGE);
+        return ($age >= self::MIN_AGE && $age <= self::MAX_YOUTH_AGE);
+    }
+
+    protected function isStudentAge($age)
+    {
+        return ($age >= self::MIN_AGE && $age <= self::MAX_STUDENT_AGE);
     }
 
     protected function youthRatePrice($tripPrice)
@@ -90,6 +96,10 @@ abstract class A_Tariff implements I_Tariff
             echo "Дополнительная услуга: второй водитель".PHP_EOL;
         };
         echo '--------------------------------------------'.PHP_EOL;
+        if ($this->errorMessage) {
+          echo $this->errorMessage.PHP_EOL;
+          return;
+        };
         echo "Базовая стоимость: {$this->baseTripPrice} руб.".PHP_EOL;
         echo "Дополнительная стоимость: {$this->additionalTripPrice} руб.".PHP_EOL;
         echo "Итого: {$this->totalTripPrice} руб.".PHP_EOL;
@@ -113,12 +123,20 @@ abstract class A_Tariff implements I_Tariff
 
     public function calculateTripPrice($distance, $hours, $minutes, $age, $isGPS = null, $isAdditionalDriver = null)
     {
+        $this->setTripProperties($distance, $hours, $minutes, $age, $isGPS, $isAdditionalDriver);
+        $this->errorMessage = "";
+
         if (!$this->isValidAge($age)) {
-            echo "К сожалению, вынуждены отказать Вам в поездке - ограничение по возрасту.";
+            $this->errorMessage = "К сожалению, вынуждены отказать Вам в поездке - ограничение по возрасту.";
+            $this->printPriceInfo();
             return -1;
         };
-        $this->setTripProperties($distance, $hours, $minutes, $age, $isGPS, $isAdditionalDriver);
+
         $this->baseTripPrice = $this->calculateBaseTripPrice();
+        if ($this->baseTripPrice < 0) {
+            $this->printPriceInfo();
+            return -1;
+        }
         $this->additionalTripPrice = $this->calculateAdditionalPrice();
         $this->totalTripPrice = $this->baseTripPrice + $this->additionalTripPrice;
 
@@ -133,7 +151,7 @@ class TariffBase extends A_Tariff
     public function __construct()
     {
         $this->tariffName = 'Тариф базовый';
-        $this->pricePerMinute = 3;
+        $this->pricePerTime = 3;
         $this->pricePerKilometer = 10;
         $this->isGpsAvailable = true;
         $this->isAdditionalDriverAvailable = false;
@@ -141,11 +159,91 @@ class TariffBase extends A_Tariff
 
     public function calculateBaseTripPrice()
     {
-        $baseTripPrice = $this->pricePerKilometer * $this->distance + $this->pricePerMinute * ($this->hours * 60 + $this->minutes);
+        $baseTripPrice = $this->pricePerKilometer * $this->distance + $this->pricePerTime * ($this->hours * 60 + $this->minutes);
 
         return $baseTripPrice;
     }
 };
 
-$tariffBase = new TariffBase();
-$tariffBase->calculateTripPrice(10, 1, 20, 20, true);
+class TariffHourly extends A_Tariff
+{
+    public function __construct()
+    {
+        $this->tariffName = 'Тариф почасовой';
+        $this->pricePerTime = 200;
+        $this->pricePerKilometer = 0;
+        $this->isGpsAvailable = true;
+        $this->isAdditionalDriverAvailable = true;
+    }
+
+    public function calculateBaseTripPrice()
+    {
+        $hours = $this->hours;
+        if ($this->minutes > 0) {
+            $hours++;
+        };
+        $baseTripPrice = $this->pricePerTime * $hours;
+
+        return $baseTripPrice;
+    }
+};
+
+class TariffDaily extends A_Tariff
+{
+    public function __construct()
+    {
+        $this->tariffName = 'Тариф почасовой';
+        $this->pricePerTime = 200;
+        $this->pricePerKilometer = 0;
+        $this->isGpsAvailable = true;
+        $this->isAdditionalDriverAvailable = true;
+    }
+
+    public function calculateBaseTripPrice()
+    {
+        $hours = $this->hours;
+        if ($this->minutes > 0) {
+            $hours++;
+        };
+        $baseTripPrice = $this->pricePerTime * $hours;
+
+        return $baseTripPrice;
+    }
+};
+
+class TariffStudent extends A_Tariff
+{
+    public function __construct()
+    {
+        $this->tariffName = 'Тариф почасовой';
+        $this->pricePerTime = 1;
+        $this->pricePerKilometer = 4;
+        $this->isGpsAvailable = true;
+        $this->isAdditionalDriverAvailable = false;
+    }
+
+    public function calculateBaseTripPrice()
+    {
+        if (!$this->isStudentAge($this->age)) {
+            $this->errorMessage = "Студенческий тариф неприменим - ограничение по возрасту";
+            return -1;
+        }
+        $hours = $this->hours;
+        if ($this->minutes > 0) {
+            $hours++;
+        };
+        $baseTripPrice = $this->pricePerTime * $hours;
+
+        return $baseTripPrice;
+    }
+};
+
+//=======================================================================================
+//$tariffBase = new TariffBase();
+//$tariffBase->calculateTripPrice(10, 1, 20, 20, true);
+
+//$tariffHour = new TariffHourly();
+//$tariffHour->calculateTripPrice(10, 1, 20, 15, true, true);
+
+$tariffStudent = new TariffStudent();
+$tariffStudent->calculateTripPrice(10, 1, 20, 27, true, true);
