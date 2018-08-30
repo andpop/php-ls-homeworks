c26cb0367a60d1b502ec5bac357afaf32047445e
Andrey Popov <andpop@mail.ru>
2018-08-30 07:27:34 +0300

    Work 4, task 1. With errors.

--- work4/task1.php
+++ work4/task1.php
@@ -9,12 +9,37 @@ abstract class A_Tariff implements I_Tariff
     protected $pricePerKilometer;   // Цена за километр
     protected $pricePerMinute;      // Цена за минуту
     protected $distance;
+    protected $hours;
+    protected $minutes;
+    protected $driverAge;
+    protected $isGPS;
+    protected $isAdditionalDriver;
+
 
     public function __construct($distance, $hours, $minutes, $driverBirthday, $isGPS = null, $isAdditionalDriver = null)
     {
         $this->distance = $distance;
+        $this->hours = $hours;
+        $this->minutes = $minutes;
+        $this->driverAge = $this->calculateAge($driverBirthday);
+        $this->isGPS = isset($isGPS);
+        $this->isAdditionalDriver = isset($this->isAdditionalDriver);
     }
 
+    /**
+     * Вычисление возраста человека по дню его рождения
+     * @param string $birthday - дата рождения в формате 'день.месяц.год'
+     * @return integer - возраст на текущую дату
+     */
+    protected function calculateAge(string $birthday)
+    {
+        $birthdayTimestamp = strtotime($birthday);
+        $age = date('Y') - date('Y', $birthdayTimestamp);
+        if (date('md', $birthdayTimestamp) > date('md')) {
+            $age--;
+        }
+        return $age;
+    }
 };
 
 class TariffBase extends A_Tariff
@@ -28,11 +53,12 @@ class TariffBase extends A_Tariff
 
     public function calculateTripPrice($distance, $hours, $minutes, $driverBirthday, $isGPS = null, $isAdditionalDriver = null)
     {
-        $tripPrice = $this->pricePerKilometer * $this->distance + $this->pricePerMinute * ($hours * 60 + $minutes);
+        $tripPrice = $this->pricePerKilometer * $this->distance + $this->pricePerMinute * ($this->$hours * 60 + $this->$minutes);
         return $tripPrice;
     }
 };
 
 $tariffBase = new TariffBase(10, 1, 20, '09.11.1974');
-echo $tariffBase->calculateTripPrice(10, 1, 20, '09.11.1974');
+//echo $tariffBase->calculateTripPrice(10, 1, 20, '09.11.1974');
+echo "Age=", $tariffBase->$driverAge;
 echo "\n";
